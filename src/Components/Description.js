4fbedca77287b4f0b4ca6beb6f900d1e6ac2444b
anis jridi <jridi.anis.2014@gmail.com>
2021-09-29 11:05:05 +0100

    first

---
+++ src/Components/Description.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Description = (props) => {
+  console.log(props);
+  // let item = list.find((el) => el.id == match.params.id);
+  return (
+    <div>
+      {/* <h1>{item.name}</h1>
+      <h3>{item.trailer}</h3>
+      <h3> {item.description}</h3> */}
+    </div>
+  );
+};
+
+export default Description;
